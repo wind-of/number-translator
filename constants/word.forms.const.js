ffaf691319b6005e945821b55c9e86de7a702dce
wind-of <adequation@mail.ru>
2020-08-02 18:56:08 +0300

    now ClassPostfixes contains postfixes (*-*)

--- constants/word.forms.const.js
+++ constants/word.forms.const.js
@@ -1,7 +1,7 @@
 const ClassPostfixes = {
-    ONE_THING_POSTFIX: ["1"], // {1}. "один миллион"
-    FEW_THINGS_POSTFIX: ["2", "3", "4"], // [2;4]. "два миллиона"
-    MANY_OR_ZERO_THINGS_POSTFIX: ["0", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16", "17", "18", '19'] // {0}u[5;9]u[11;19]. "пять миллионов"
+    ONE_THING_POSTFIX: "",
+    FEW_THINGS_POSTFIX: "а",
+    MANY_OR_ZERO_THINGS_POSTFIX: "ов" 
 }
 const NonIntegers = {
   ENDS_WITH_ONE: 0, // {1}. // "одна сотая", "двести двадцать одна сотая", ...
