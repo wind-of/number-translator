1f92327f50e46e922a296a6297f3187b102666c0
wind-of <adequation@mail.ru>
2020-08-03 09:27:22 +0300

    version: 1.3.1

--- test/errors.test.js
+++ test/errors.test.js
@@ -1,4 +1,4 @@
-const wordFrom = require("../index");
+const { translateNumber } = require("../index");
 const { errors } = require("../errors");
 test("Non-numeric values", () => {
   ["f", "", [], {}, null, NaN, undefined]
@@ -7,5 +7,5 @@ test("Non-numeric values", () => {
 
 test("Unsafe numbers", () => {
   [192530257132532235671230, 2197501242652579812918, 131515215215138398384438, 3168587878578921357231123, 215363246734587734623, 4326324634234234643263, 125784215140320521251, 215215220547513531515, 1e23]
-    .forEach(arg => expect(wordFrom(arg)).toBe(errors.NotSafeNumber))
+    .forEach(arg => expect(translateNumber(arg)).toBe(errors.NotSafeNumber))
 })
