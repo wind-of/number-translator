dce2bb1465b620ad2b0a918ea6370278f3d7f55d
wind-of <adequation@mail.ru>
2021-07-30 08:46:44 +0300

    minor change

--- validation/errors.js
+++ validation/errors.js
@@ -1,6 +1,6 @@
 const errors = {
   NOT_SAFE_NUMBER:
-  `Passed number is not safe. Available numbers are numbers in range [${Number.MIN_SAFE_INTEGER}; ${Number.MAX_SAFE_INTEGER}].
+  `Passed number is not safe. Safe numbers are numbers in range [${Number.MIN_SAFE_INTEGER}; ${Number.MAX_SAFE_INTEGER}].
   You can pass the number wrapped in quotes to avoid this limitation.`,
   NOT_A_NUMBER: "Passed value is not a number.",
   NON_NUMERIC_SYMBOLS: "There are non-numeric symbols in the passed string.",
