0924c4123816ffcd181f7ae0c847ac771785aeab
wind-of <adequation@mail.ru>
2024-12-07 08:31:24 +0300

    ci: проверка прекоммита #11

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-const { getCategoryWordForNonIntegerPart } = require("./numbers/word.specific.getters");
-const { translateThreeOrLessDigitNumber } = require("./translators/translate.small.number");
-const { getTranslatedNumberWithClassWord } = require("./translators/translate.number.triplet");
-const { removeLastNSymbols } = require("./utils/remove-last-n-symbols");
-const { lastTwoChars } = require("./utils/two-last-digits");
-const { NonIntegersPostfixes } = require("./constants/word.postfixes.const");
+const { getCategoryWordForNonIntegerPart } = require("./numbers/word.specific.getters")
+const { translateThreeOrLessDigitNumber } = require("./translators/translate.small.number")
+const { getTranslatedNumberWithClassWord } = require("./translators/translate.number.triplet")
+const { removeLastNSymbols } = require("./utils/remove-last-n-symbols")
+const { lastTwoChars } = require("./utils/two-last-digits")
+const { NonIntegersPostfixes } = require("./constants/word.postfixes.const")
 const { OUTPUT_TYPE_ERROR, OUTPUT_TYPE_VALID } = require("./constants/output.types")
-const { findError } = require("./validation/is-valid-number");
-const { prepareNumber } = require("./utils/prepare-number");
+const { findError } = require("./validation/is-valid-number")
+const { prepareNumber } = require("./utils/prepare-number")
 
-const output = (message, type) => ({message, type})
+const output = (message, type) => ({ message, type })
 
 function translateNumber(number) {
-  const error = findError(number);
-  if(error) return output(error, OUTPUT_TYPE_ERROR)
+  const error = findError(number)
+  if (error) return output(error, OUTPUT_TYPE_ERROR)
 
-  if(number < 0) {
+  if (number < 0) {
     return `минус ${translateNumber(number.toString().slice(1))}`
   }
 
-  number = prepareNumber(number);
-  
-  
-  if(number.includes(".")) {
-    const [integerPart, nonIntegerPart] = number.split(".");
-    const integerPartTranslation = translateNumber(integerPart);
-    const translatedNonIntegerPart = translateNonIntegerPart(nonIntegerPart);
+  number = prepareNumber(number)
+
+  if (number.includes(".")) {
+    const [integerPart, nonIntegerPart] = number.split(".")
+    const integerPartTranslation = translateNumber(integerPart)
+    const translatedNonIntegerPart = translateNonIntegerPart(nonIntegerPart)
     const integerPartEndsWithOne = integerPartTranslation.substring(integerPartTranslation.length - 4) === "один"
-    if(translatedNonIntegerPart === "") {
+    if (translatedNonIntegerPart === "") {
       return integerPartTranslation
     }
-    if(integerPartEndsWithOne) {
+    if (integerPartEndsWithOne) {
       return `${integerPartTranslation.substring(0, integerPartTranslation.length - 2) + "на"} целая ${translatedNonIntegerPart}`
     }
     return `${integerPartTranslation} целых ${translatedNonIntegerPart}`
   }
 
-  if(number < 1000) {
+  if (number < 1000) {
     return translateThreeOrLessDigitNumber(number)
   }
 
-  const translatedParts = [];
-  for(let classIndex = 0;; classIndex++, number = removeLastNSymbols(number, 3)) {
-    if(number.length < 4) {
-      const wordFromNumber = getTranslatedNumberWithClassWord(number, classIndex);
-      if (wordFromNumber) { translatedParts.unshift(wordFromNumber)
-
-       }
+  const translatedParts = []
+  for (let classIndex = 0; ; classIndex++, number = removeLastNSymbols(number, 3)) {
+    if (number.length < 4) {
+      const wordFromNumber = getTranslatedNumberWithClassWord(number, classIndex)
+      if (wordFromNumber) {
+        translatedParts.unshift(wordFromNumber)
+      }
       break
     }
-    const triplet = number.slice(number.length - 3, number.length);
-    const wordFromTriplet = getTranslatedNumberWithClassWord(triplet, classIndex);
-    if(classIndex === 0 && wordFromTriplet === "ноль") {
+    const triplet = number.slice(number.length - 3, number.length)
+    const wordFromTriplet = getTranslatedNumberWithClassWord(triplet, classIndex)
+    if (classIndex === 0 && wordFromTriplet === "ноль") {
       continue
     }
-    if(wordFromTriplet) {      translatedParts.unshift(wordFromTriplet)
+    if (wordFromTriplet) {
+      translatedParts.unshift(wordFromTriplet)
     }
   }
   return translatedParts.join(" ")
@@ -63,36 +63,35 @@ function translateNumber(number) {
  * @function translateNonIntegerPart
  * Gets a special word of the some category of the number.
  * @param {String} number
- * A string number. 
- * 
+ * A string number.
+ *
  * @return {String} Translated non-integer part.
  */
 function translateNonIntegerPart(number) {
-  if(Number(number) === 0) {
+  if (Number(number) === 0) {
     return ""
   }
 
-  for(let i = number.length - 1; number[i] === "0"; i--)
-    if(number[i - 1] !== "0")
-      number = number.substring(0, i)
+  for (let i = number.length - 1; number[i] === "0"; i--) if (number[i - 1] !== "0") number = number.substring(0, i)
 
-  const translated = translateNumber(number);
-  const lastTwoDigits = lastTwoChars(number);
-  const lastDigit = lastTwoDigits[1] || lastTwoDigits[0];
-  const nonIntegerPartType = lastDigit === "1" && lastTwoDigits !== "11" 
-      ? NonIntegersPostfixes.ENDS_WITH_ONE 
-      : NonIntegersPostfixes.OTHERWISE;
+  const translated = translateNumber(number)
+  const lastTwoDigits = lastTwoChars(number)
+  const lastDigit = lastTwoDigits[1] || lastTwoDigits[0]
+  const nonIntegerPartType =
+    lastDigit === "1" && lastTwoDigits !== "11" ? NonIntegersPostfixes.ENDS_WITH_ONE : NonIntegersPostfixes.OTHERWISE
 
   return (
-    nonIntegerPartType === NonIntegersPostfixes.ENDS_WITH_ONE
+    (nonIntegerPartType === NonIntegersPostfixes.ENDS_WITH_ONE
       ? removeLastNSymbols(translated, 4) + "одна"
-      : translated
-    ) + ` ${getCategoryWordForNonIntegerPart(number.length)}` + nonIntegerPartType
+      : translated) +
+    ` ${getCategoryWordForNonIntegerPart(number.length)}` +
+    nonIntegerPartType
+  )
 }
- 
-module.exports = { 
+
+module.exports = {
   translateNumber: (input) => {
     const output_ = translateNumber(input)
-    return output_.message ? output_ : output(output_, OUTPUT_TYPE_VALID) 
-  }
+    return output_.message ? output_ : output(output_, OUTPUT_TYPE_VALID)
+  },
 }
