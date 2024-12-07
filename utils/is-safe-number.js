a4ff91213be9b61fad7605ae4c780a5b1784faa9
wind-of <adequation@mail.ru>
2024-12-07 08:24:10 +0300

    ci: проверка прекоммита #3

--- utils/is-safe-number.js
+++ utils/is-safe-number.js
@@ -2,7 +2,8 @@ module.exports = {
   isSafeNumber(number) {
     return typeof number === "string" 
       ||  (
-            number <= Number.MAX_SAFE_INTEGER &&
+            number <= Number.MAX_SAFE_INTEGER 
+            &&
             number >= Number.MIN_SAFE_INTEGER
           ) 
   }
